5e2dc2935f840ad6bc398c153c041fa17f866914
hawkup <spiritsquare@gmail.com>
2020-12-07 04:01:32 +0700

    Add /posts and /posts/:id

---
+++ nuxt/pages/posts/_id.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ post.title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {}
+    }
+  },
+  async fetch() {
+    this.post = await fetch(`https://api.nuxtjs.dev/posts/${this.$route.params.id}`).then(res =>
+      res.json()
+    )
+    console.log(this.post)
+  }
+}
+</script>
---
+++ nuxt/pages/posts/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div v-for="post in posts" :key="post.id">
+      <nuxt-link v-bind:to="'/posts/' + post.id">
+        <h1>{{ post.title }}</h1>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: []
+    }
+  },
+  async fetch() {
+    this.posts = await fetch('https://api.nuxtjs.dev/posts').then(res =>
+      res.json()
+    )
+  }
+}
+</script>
